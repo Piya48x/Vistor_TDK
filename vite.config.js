5b3a75cfc07817cda5962edde6bd8f042f62affa
Piya48z <pdpd4650z@gmail.com>
2025-08-27 19:27:45 +0700

    sdfa

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs' // เพิ่มตรงนี้
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // ให้ dev server ฟังทุก interface
-    port: 3000, // หรือพอร์ตที่ใช้
-    https: {
-      key: fs.readFileSync('./192.168.0.181+1-key.pem'),
-      cert: fs.readFileSync('./192.168.0.181+1.pem'),
-    },
+    host: true,
+    port: 3000,
     allowedHosts: [
-      '00c286eddb3c.ngrok-free.app', // เพิ่ม URL ของ ngrok
+      'vistor-tdk-oqni.vercel.app',
       'localhost',
       '127.0.0.1'
     ]
   }
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import fs from 'fs' // เพิ่มตรงนี้
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true, // ให้ dev server ฟังทุก interface
+//     port: 3000, // หรือพอร์ตที่ใช้
+//     https: {
+//       key: fs.readFileSync('./192.168.0.181+1-key.pem'),
+//       cert: fs.readFileSync('./192.168.0.181+1.pem'),
+//     },
+//     allowedHosts: [
+//       '00c286eddb3c.ngrok-free.app', // เพิ่ม URL ของ ngrok
+//       'localhost',
+//       '127.0.0.1'
+//     ]
+//   }
+// })
